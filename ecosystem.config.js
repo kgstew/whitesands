895650907db77977d36ff534d2f7be130a89b2a5
Kyle Stewart <kgstew@gmail.com>
2018-01-09 21:30:17 -0800

    Adding pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'whitesands',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-34-226-217-209.compute-1.amazonaws.com',
+      key: '~/.ssh/whitesands_server.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:kgstew/whitesands.git',
+      path: '/home/ubuntu/whitesands',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
